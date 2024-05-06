35ea9eda57b7d05c60be59ad46159e25a78b921c
TitovaSolomia <titovasolomia@icloud.com>
2024-05-06 18:16:57 +0200

    2024-05-06 ST

---
+++ app/routes.py
@@ -0,0 +1,33 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template("index.html.jinja")
+
+
+@app.route('/extract')
+def extract():
+    return render_template("extract.html.jinja")
+
+
+@app.route('/products')
+def products():
+    return render_template("products.html.jinja")
+
+
+@app.route('/author')
+def author():
+    return render_template("author.html.jinja")
+
+
+@app.route('/product/<name>')
+def product_id():
+    return render_template("product.html.jinja", product_id = product_id)
+
+
+
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name = "World"):
+    return f"Hello, {name}!"
