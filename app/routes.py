03cda7d0981f83b91b227c95280f1da599430cf7
TitovaSolomia <titovasolomia@icloud.com>
2024-05-13 18:15:23 +0200

    2024-05-13 ST

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,21 @@
 from app import app
-from flask import render_template
+import requests
+from flask import render_template, request, redirect, url_for
 
 @app.route('/')
 def index():
     return render_template("index.html.jinja")
 
 
-@app.route('/extract')
+@app.route('/extract', methods=['POST', 'GET'])
 def extract():
+    if request.method == 'POST':
+        product_id = request.form.get('product_id')
+        url = f'https://www.ceneo.pl/{product_id}'
+        response = requests.get(url)
+        if response.status_code == requests.codes['ok']:
+            #proces ekstrakcji
+            return redirect(url_for('product', product_id = product_id)) 
     return render_template("extract.html.jinja")
 
 
@@ -21,8 +29,8 @@ def author():
     return render_template("author.html.jinja")
 
 
-@app.route('/product/<name>')
-def product_id():
+@app.route('/product/<product_id>')
+def product(product_id):
     return render_template("product.html.jinja", product_id = product_id)
 
 
